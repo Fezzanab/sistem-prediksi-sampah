894b3184ea5bc45488aeb10f726720a45c490e22
fezzanab <anabfaez@gmail.com>
2026-06-23 03:32:50 +0700

    update fitur

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     
     # Database Configuration (MySQL as primary, fallback to SQLite if MySQL env is missing)
     DB_USER = os.environ.get("DB_USER", "root")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD", "@Akucinta17")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
     DB_HOST = os.environ.get("DB_HOST", "localhost")
     DB_PORT = os.environ.get("DB_PORT", "3306")
     DB_NAME = os.environ.get("DB_NAME", "sistem_prediksi_sampah")
